a5fd0789bb0f4627ae9a93e8e4830e68a07cbbce
rayhan360 <hossainrayhan897@gmail.com>
2023-12-22 12:17:41 +0600

    update task status to complete

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
       res.send(result)
     })
     // post task data
+    app.get("/task/complete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.findOne(query);
+      res.send(result)
+    })
+    // post task data
     app.post("/task", async (req, res) => {
       const tasks = req.body;
       const result = await taskCollection.insertOne(tasks);
@@ -60,6 +67,23 @@ async function run() {
       const result = await taskCollection.updateOne(
         filter,
         changedStatusOnGoing
+      );
+      res.send(result)
+    })
+
+    app.patch("/task/complete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+
+      const changedStatusComplete = {
+        $set: {
+          status: "completed"
+        },
+      };
+
+      const result = await taskCollection.updateOne(
+        filter,
+        changedStatusComplete
       );
       res.send(result)
     })
