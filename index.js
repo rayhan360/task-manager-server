1d9395e28c3d602af7aecdba78ac0812cab10b65
rayhan360 <hossainrayhan897@gmail.com>
2023-12-22 10:24:32 +0600

    get data to mongodb id based

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 // middleware
@@ -33,12 +33,21 @@ async function run() {
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.get("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.findOne(query);
+      res.send(result)
+    })
     // post task data
     app.post("/task", async (req, res) => {
       const tasks = req.body;
       const result = await taskCollection.insertOne(tasks);
       res.send(result);
     });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
